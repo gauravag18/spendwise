53b9ab412cf919b782a1dd65c25c0b6609fd620d
Gaurav Agarwalla <agarwalgaurav1809@gmail.com>
2025-08-31 18:16:04 +0530

    authentication changes

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -1,30 +1,31 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser } from '@clerk/nextjs/server';
 import { db } from './db';
 
 export const checkUser = async () => {
     const user = await currentUser();
+
     if (!user) {
         return null;
     }
 
     const loggedInUser = await db.user.findUnique({
-        where :{
-            clerkUserId:user.id,
+        where: {
+            clerkUserId: user.id,
         },
     });
-     
-    if(loggedInUser){
+
+    if (loggedInUser) {
         return loggedInUser;
     }
 
     const newUser = await db.user.create({
-        data :{
-            clerkUserId:user.id,
-            name:`${user.firstName} ${user.lastName}`,
-            imageUrl:user.imageUrl,
-            email:user.emailAddresses[0]?.emailAddress
-        }
-    })
-    
+        data: {
+            clerkUserId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0]?.emailAddress,
+        },
+    });
+
     return newUser;
-}
+};
